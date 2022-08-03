29cabf36ec3e855ccb76262f9bb02bd6c401c413
Max Humber <max.humber@gmail.com>
2022-08-03 09:51:01 -0400

    Scroll control + font sizing

--- Ponkan/Interface/Views/Transcribe/NewTranscribeView.swift
+++ Ponkan/Interface/Views/Transcribe/NewTranscribeView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct NewTranscribeView: View {
     @Environment(\.colorScheme) var colorScheme: ColorScheme
     @StateObject private var viewModel: NewTranscribeViewModel
+    @State private var fontSize: Double = 25
     
     init(_ text: String = "", active: Bool = false) {
         self._viewModel = StateObject(wrappedValue: .init(text, active: active))
@@ -13,31 +14,46 @@ struct NewTranscribeView: View {
             Image(systemName: "circle.fill")
                 .foregroundColor(.red)
                 .opacity(viewModel.listening ? 1 : 0)
-            ScrollView(showsIndicators: false) {
-                VStack(alignment: .leading, spacing: 20) {
-                    ForEach(viewModel.history, id: \.self) { text in
-                        Text(text)
+            ScrollViewReader { reader in
+                ScrollView(showsIndicators: false) {
+                    VStack(alignment: .leading, spacing: 20) {
+                        ForEach(viewModel.history, id: \.self) { text in
+                            Text(text)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .foregroundColor(.secondary)
+                        }
+                        Text(viewModel.current)
                             .frame(maxWidth: .infinity, alignment: .leading)
+                            .id("LAST")
                     }
-                    Text(viewModel.current)
-                        .frame(maxWidth: .infinity, alignment: .leading)
+                    .font(.system(size: fontSize))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
                 }
-                .font(.title)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.horizontal)
+                .onChange(of: viewModel.current) { _ in
+                    reader.scrollTo("LAST")
+                }
             }
-            HStack(spacing: 20) {
-                Button {
-                    viewModel.toggle()
-                } label: {
-                    Text("Start/Stop")
+            VStack {
+                HStack {
+                    Text("10")
+                    Slider(value: $fontSize, in: 20...40)
+                    Text("30")
                 }
-                Button {
-                    viewModel.clear()
-                } label: {
-                    Text("Clear")
+                .padding()
+                HStack(spacing: 20) {
+                    Button {
+                        viewModel.toggle()
+                    } label: {
+                        Text("Start/Stop")
+                    }
+                    Button {
+                        viewModel.clear()
+                    } label: {
+                        Text("Clear")
+                    }
+                    .foregroundColor(.red)
                 }
-                .foregroundColor(.red)
             }
         }
     }
